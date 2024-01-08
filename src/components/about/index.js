7abeb99456088736b6c98154d3b1db903f481433
Wagieh Ahmed <wagehahmed1237@gmail.com>
2024-01-09 00:49:43 +0200

    p3

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -17,7 +17,9 @@ import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 import LocalShippingIcon from "@mui/icons-material/LocalShipping";
 import HeadsetMicIcon from "@mui/icons-material/HeadsetMic";
 import { SectionSubTitle, SectionTitle, TitlesContainer } from "../../Styles/common";
-function About({ reff }) {
+
+export default  React.forwardRef(({reff})=>{
+  
   const matches = useMediaQuery(theme.breakpoints.down("md"));
   return (
     <Container ref={reff}>
@@ -62,7 +64,9 @@ function About({ reff }) {
         </AboutItemContent>
       </AboutItem>
     </Container>
-  );
-}
+  )
 
-export default  React.forwardRef(About);
+
+})
+
+
