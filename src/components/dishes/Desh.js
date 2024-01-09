4e29ae97bd1a6c0c734c32f76e94f4ac3e1d1cd6
Wagieh Ahmed <wagehahmed1237@gmail.com>
2024-01-09 20:58:38 +0200

    hover

--- src/components/dishes/Desh.js
+++ src/components/dishes/Desh.js
@@ -1,9 +1,25 @@
 import React from "react";
-import { DeshActions, DeshActionsAddToCard, DeshActionsPrice, DeshCard, DeshCardMedia, DeshContainer, DeshName } from "../../Styles/deshes";
+import { DeshActions, DeshActionsAddToCard, DeshActionsPrice, DeshCard, DeshCardMedia, DeshContainer, DeshName ,IconContainer} from "../../Styles/deshes";
 import {
   Rating,
 } from "@mui/material";
 import { DeshCardContent } from './../../Styles/deshes/index';
+
+
+
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default function Desh({item,matches}) {
  
@@ -21,6 +37,16 @@ export default function Desh({item,matches}) {
           <DeshActionsPrice variant="text">{item.price}</DeshActionsPrice>
           <DeshActionsAddToCard size="small">add to card</DeshActionsAddToCard>
         </DeshActions>
+
+        <IconContainer matches={true} className="addToCard">
+              <ShoppingCartIcon />
+          </IconContainer>
+
+
+
+
+
+
       </DeshCard>
     </DeshContainer>
   );
